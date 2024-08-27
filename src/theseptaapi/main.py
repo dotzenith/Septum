a0f5835a22dde1c03d6fc03d8a07101e1c08429b
dotzenith <dotzenith@gmail.com>
2024-08-26 23:47:19 -0400

    remove examples

--- src/theseptaapi/main.py
+++ src/theseptaapi/main.py
@@ -23,13 +23,6 @@ def get_stations():
         A list of dictionaries where `station_name` is the colloquial station name and
             `parameter` is what the API expects as input for the particular station.
 
-    Example:
-        [
-            {"station_name": "49th Street", "parameter": "49th St"},
-            {"station_name": "Holmesburg Junction", "parameter": "Holmesburg Jct"},
-            {"station_name": "Fern Rock Transportation Center", "parameter": "Fern Rock TC"},
-        ]
-
     Note:
         These are NOT the same the stations returned by `/schedule/stations` endpoint.
         Those are for an API that is not publicly documented. See `/schedule/stations` for more info.
@@ -46,13 +39,6 @@ def get_bus_routes():
     Returns:
         A list of dictionaries where `route_name` is the colloquial name of a given bus route,
             and `route_number` (not always a "number") is what the API expects.
-
-    Example:
-        [
-            {"route_number": "1", "route_name": "parx casino to 54th-city"},
-            {"route_number": "103", "route_name": "Ardmore to 69th Street Transportation Center"},
-            {"route_number": "104", "route_name": "West Chester University to 69th Street Transportation Center"}
-        ]
     """
     return scrapers.get_bus_routes()
 
@@ -65,13 +51,6 @@ def get_trolley_routes():
     Returns:
         A list of dictionaries where `route_name` is the colloquial name of a given trolley route,
             and `route_number` is what the API expects.
-
-    Example:
-        [
-            {"route_number": "10", "route_name": "13th-Market to 63rd-Malvern"},
-            {"route_number": "11", "route_name": "13th-Market to Darby Transportation Center"},
-            {"route_number": "15", "route_name": "63rd-Girard to Richmond-Westmoreland"},
-        ]
     """
     return scrapers.get_trolley_routes()
 
@@ -84,19 +63,12 @@ def get_lines():
 
     Returns:
         A list of dictionaries, each containing a `line_code` and a `line_name`
-
-    Example:
-        [
-            {"line_code": "AIR", "line_name": "Airport"},
-            {"line_code": "CHE", "line_name": "Chestnut Hill East"},
-            {"line_code": "CHW", "line_name": "Chestnut Hill West"}
-        ]
     """
     return schedule.get_lines()
 
 
 @app.get("/schedule/stations", response_model=list[ScheduleStationOuput])
-def get_stattions_for_lines(line: Annotated[StationInput, Depends()]):
+def get_stations_for_lines(line: Annotated[StationInput, Depends()]):
     """
     Retrieve a list of stations for a specific line.
 
@@ -108,15 +80,6 @@ def get_stattions_for_lines(line: Annotated[StationInput, Depends()]):
 
     Returns:
         A list of dictionaries, each containing a stop_id and stop_name.
-
-    Example:
-        Request: GET /schedule/stations?line=TRE
-        Response:
-        [
-            {"stop_id": "90701", "stop_name": "Trenton"},
-            {"stop_id": "90004", "stop_name": "Gray 30th Street"},
-            {"stop_id": "90007", "stop_name": "Temple University"}
-        ]
 
     Note:
         The line parameter is automatically validated against the list of
@@ -149,20 +112,6 @@ def get_schedule_for_station(query: Annotated[ScheduleInput, Depends()]):
                 - arrival_time (str, optional): The arrival time at the dest station.
                     (only present if both origin and destination are specified).
                 - train_id (str): The unique identifier for the train.
-
-    Example:
-        Request: GET /schedule?line=TRE&direction=0&orig=Trenton&dest=Gray 30th Street&direction=0
-        Response:
-        {
-            "weekday": [
-                {"departure_time": "06:59:00", "arrival_time": "07:47:00", "train_id": "7404"},
-                {"departure_time": "07:26:00", "arrival_time": "08:20:00", "train_id": "9744"}
-            ],
-            "weekend": [
-                {"departure_time": "13:10:00", "arrival_time": "13:58:00", "train_id": "718"},
-                {"departure_time": "15:05:00", "arrival_time": "15:53:00", "train_id": "722"}
-            ]
-        }
 
     Note:
         If only the origin station is provided, the schedule will show departure times only.
